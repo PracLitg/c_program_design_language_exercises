11454aafbba44b9a02b84860d1472d14050b611a
pracli <756629046@qq.com>
2016-11-05 01:31:51 +0800

    new exercises of pro-6-1

--- libraries/iolines/iolines.c
+++ libraries/iolines/iolines.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAXLINE 1000
 #define MAXLEN 1000
@@ -21,6 +22,70 @@ void ungetch(int c) {
         printf("Ungetch: Too many characters\n");
     else
         buf[bufp++] = c;
+}
+
+
+/*
+ * 功能: 从输入中读取下一个单词或字符,具有下划线处理,识别注释等功能
+ * return type: 单词首字符
+ */
+int getword(char* word, int lim) {
+    int c;
+    char* w = word;
+
+    while (isspace(c = getch()))
+        ;
+
+    if (c != EOF) {
+        *w++ = c;
+    }
+    if (isalpha(c) || c == '#' || c == '_') {
+        for (; --lim > 0; w++) {
+            if (!isalnum(*w = getch()) || *w == '_') {
+                ungetch(*w);
+                break;
+            }
+        }
+    } else if (c == '\'') {
+        while (--lim > 0) {
+            if ((*w++ = getch()) == '\'') {
+                break;
+            }
+            if (*w == EOF) {
+                ungetch(*w);
+                break;
+            }
+        }
+    } else if (c == '\"') {
+        while (--lim > 0) {
+            if ((*w++ = getch()) == '\"') {
+                break;
+            }
+            if (*w == EOF) {
+                ungetch(*w);
+                break;
+            }
+        }
+    } else if (c == '/') {
+        int d;
+        if ((d = getch()) == '*') {
+            while ((d = getch()) != EOF) {
+                if (d == '*') {
+                    int t;
+                    if ((t = getch()) == '/') {
+                        break;
+                    } else {
+                        ungetch(t);
+                    }
+                }
+            }
+        } else {
+            ungetch(d);
+        }
+    }
+
+    *w = '\0';
+    return c;
 }
 
 
